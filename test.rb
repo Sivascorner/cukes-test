fd2df6da725b7a2e61da57817f706773b0f1faba
Sivasankar Subramanian <sivsubra@Sivasankars-MBP.fios-router.home>
2016-10-25 18:28:05 -0400

    Modified test.rb

--- test.rb
+++ test.rb
@@ -1,3 +1,21 @@
-puts "This is test"
-string = "Number"
-"Number".length()
+# puts "This is test"
+# string = "Number"
+# "Number".length()
+### ====
+class Song
+  attr_reader :name, :age, :height
+  def initialize(name,age,height)
+    @name = name
+    @age = age
+    @height = height
+    #puts @name,@age,@height
+  end
+
+
+end
+
+test_song = Song.new("Siva","34","5.5")
+puts test_song.name
+# test_song.name
+# test_song.age
+# test_song.height
