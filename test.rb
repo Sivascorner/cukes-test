b38b4dd2f6635e7c6714bf20e9f566b371f6a836
Sivasankar Subramanian <sivsubra@Sivasankars-MBP.fios-router.home>
2016-10-25 23:56:13 -0400

    New info

--- test.rb
+++ test.rb
@@ -2,20 +2,39 @@
 # string = "Number"
 # "Number".length()
 ### ====
-class Song
-  attr_reader :name, :age, :height
-  def initialize(name,age,height)
-    @name = name
-    @age = age
-    @height = height
-    #puts @name,@age,@height
-  end
-
-
-end
-
-test_song = Song.new("Siva","34","5.5")
-puts test_song.name
+# class Song
+#   attr_reader :name, :age, :height
+#   def initialize(name,age,height)
+#     @name = name
+#     @age = age
+#     @height = height
+#     #puts @name,@age,@height
+#   end
+#
+#
+# end
+#
+# test_song = Song.new("Siva","34","5.5")
+# puts Song.inspect
 # test_song.name
 # test_song.age
 # test_song.height
+
+# ====
+# Case : 3
+
+class Song
+  attr_reader :duration
+
+  attr_writer :height
+  MAX_DURATION  = 60
+def how_long(duration)
+  @new_duration = duration
+  return @new_duration.to_i > MAX_DURATION
+
+end
+end
+song1 = Song.new()
+
+puts song1.how_long(61)
+song1.height
